44ce792d10d1bfcf81a3a01d6dd94642bb784714
camilabersan <camilabersan.564@gmail.com>
2023-08-07 14:19:45 -0300

    feat: showcontent in html

--- assets/js/script.index.js
+++ assets/js/script.index.js
@@ -6,6 +6,8 @@ function savePost(){
     const publisher = document.getElementById("publisher").value;
     const date= document.getElementById("date").value;
 
+    console.log(title,resume,publisher,date);
+
     if(title && resume && publisher && date){
         storePost(title, resume, publisher, date);
     }
@@ -14,9 +16,33 @@ function savePost(){
 
 function storePost(title, resume, publisher, date){
     const post = {
-        title: title,
-        resume: resume,
-        publisher: publisher,
+        title,
+        resume,
+        publisher,
         date: date
-    }
+    };
+
+    posts.push(post);
+    showPosts();
+    
+}
+
+function showPosts(){
+    let showContent = "";
+
+    posts.forEach((post, index) => {
+        showContent += `
+            <div class="post"> 
+                <h2>${post.title}</h2>
+                <p><strong>Resumo:</strong>${post.resume}</p>
+                <p><strong>Autor:</strong>${post.publisher}</p>
+                <p><strong>Data de publicação:</strong>${post.date}</p>
+
+                <button onclick="editPost(${index})">Editar</button>
+                <button onclick="removePost(${index})">Remover</button>
+            </div>
+        `;
+    })
+
+    document.getElementById("list").innerHTML = showContent;
 }
